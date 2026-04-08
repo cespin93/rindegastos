16df16cdf0cbe03b71feb45a7f717ac8a2d2b9f3
cespin93 <cespin93@gmail.com>
2026-04-08 16:08:43 -0400

    Actualizar carpeta Drive

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   CLIENT_ID:      '455263097487-8eo55fa15bvq0ctcnhi1l8clet3pctp0.apps.googleusercontent.com',
   SHEET_ID:       '1YCr7t4W0WMMRa4e_jS8vN4K8QNGrqYhgXxjakL4YJPU',
-  DRIVE_FOLDER_ID:'1se5o3D9lkI4jQOkWdiBPXvpnypcf83PB',
+  DRIVE_FOLDER_ID:'1F6BHuiP1p1d2-4Kil09VnNxYnF0SpaTn',
   RECEIPTS_EMAIL: '', // ← Pon aquí el Gmail que crees para recibir comprobantes
   SCOPES: [
     'https://www.googleapis.com/auth/spreadsheets',
