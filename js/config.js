74fe63059c9ead1432c225d15fabc035fee52466
cespin93 <cespin93@gmail.com>
2026-04-08 14:07:07 -0400

    Actualizar Client ID

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  CLIENT_ID:      '505225600131-4t7fvrr1vur95sn2g0ve0k4kgg6tgqj6.apps.googleusercontent.com',
+  CLIENT_ID:      '455263097487-8eo55fa15bvq0ctcnhi1l8clet3pctp0.apps.googleusercontent.com',
   SHEET_ID:       '1YCr7t4W0WMMRa4e_jS8vN4K8QNGrqYhgXxjakL4YJPU',
   DRIVE_FOLDER_ID:'1se5o3D9lkI4jQOkWdiBPXvpnypcf83PB',
   RECEIPTS_EMAIL: '', // ← Pon aquí el Gmail que crees para recibir comprobantes
