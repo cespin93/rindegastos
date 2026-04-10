28b9ff9255e0962ec99d8a31cce5893cc339c64a
cespin93 <cespin93@gmail.com>
2026-04-10 12:24:49 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   CLIENT_ID:       '455263097487-8eo55fa15bvq0ctcnhi1l8clet3pctp0.apps.googleusercontent.com',
   SHEET_ID:        '1YCr7t4W0WMMRa4e_jS8vN4K8QNGrqYhgXxjakL4YJPU',
   DRIVE_FOLDER_ID: '1F6BHuiP1p1d2-4Kil09VnNxYnF0SpaTn',
-  GEMINI_API_KEY:  'AIzaSyCOalxhJReAGDWvzd28hvzBcPhTmwCYyJk',
+  GEMINI_API_KEY:  'AIzaSyD5vkdwM0icKDVktLttsw42xZf5XW2QQvU',
   RECEIPTS_EMAIL:  '', // ← Pon aquí el Gmail que crees para recibir comprobantes
   SCOPES: [
     'https://www.googleapis.com/auth/spreadsheets',
