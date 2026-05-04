53b0645c6d9c413f8c002cfa45ba9d6838ed45ff
cespin93 <cespin93@gmail.com>
2026-05-04 16:16:11 -0400

    Actualiza URL del Apps Script (nuevo despliegue con doGet)

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   // ← Pega aquí la URL del web app después de desplegar el Apps Script
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzUmTXNdko96Mt_eATyE9hc49YBeCD67uoUlLKpqstTmxSvIPk8DfYwbCiozNEEAJU-Og/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxC9eencfqLa4OdMOZcya4yc754MqjQiQgKvphMxugxl7EH7aED7LErbi9w141fTVasBQ/exec',
   RECEIPTS_EMAIL:  '' // Email para recibir comprobantes (opcional)
 };
