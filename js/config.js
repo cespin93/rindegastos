cc00ad600c5fc6c4755d4b9ab26201ab601f9336
cespin93 <cespin93@gmail.com>
2026-05-04 16:27:53 -0400

    Actualiza URL Apps Script (nuevo despliegue desde cero)

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   // ← Pega aquí la URL del web app después de desplegar el Apps Script
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxC9eencfqLa4OdMOZcya4yc754MqjQiQgKvphMxugxl7EH7aED7LErbi9w141fTVasBQ/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbygZo3GPcmSTMPGZOVOKiGFN8Mbvnop3IDN6c7943o1wVj23iZaHh-dP9022qXlNM3jsw/exec',
   RECEIPTS_EMAIL:  '' // Email para recibir comprobantes (opcional)
 };
