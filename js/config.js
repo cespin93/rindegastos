7fb92641142b018cfe8559c807e91e17959e56f9
cespin93 <cespin93@gmail.com>
2026-05-05 10:24:51 -0400

    Update Apps Script URL to diag-2

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   // ← Pega aquí la URL del web app después de desplegar el Apps Script
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbygZo3GPcmSTMPGZOVOKiGFN8Mbvnop3IDN6c7943o1wVj23iZaHh-dP9022qXlNM3jsw/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyesD8EuF3Ao_ufxWA9bVRed4vtWEmw17z4nYZwBxFPGU9VPIpb7aGNZ_EV52rvw1nlWw/exec',
   RECEIPTS_EMAIL:  '' // Email para recibir comprobantes (opcional)
 };
